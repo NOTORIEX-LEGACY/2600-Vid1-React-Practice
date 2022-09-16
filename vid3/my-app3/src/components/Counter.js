e1adfe3b625db894f245c583aca732ecc93522ea
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-09-16 00:01:41 +0000

    vid3 wip

---
+++ vid3/my-app3/src/components/Counter.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>You clicked {} times</p>
+            <button onClick = ""></button>
+        </div>
+    )
+}
